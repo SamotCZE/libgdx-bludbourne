faf852619926dab596507ecfb928d90a5ac7f537
SamotCZE <thomas.hudecek@gmail.com>
2016-03-22 23:14:30 +0100

    Util class, Map manager class, main screen class

--- desktop/src/cz/hudydev/libgdx/bludbourne/desktop/DesktopLauncher.java
+++ desktop/src/cz/hudydev/libgdx/bludbourne/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package cz.hudydev.libgdx.bludbourne.desktop;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import cz.hudydev.libgdx.bludbourne.BludBourneGame;
@@ -7,6 +9,15 @@ import cz.hudydev.libgdx.bludbourne.BludBourneGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new BludBourneGame(), config);
+
+		config.title = "BludBourne";
+		config.useGL30 = false;
+		config.width = 800;
+		config.height = 600;
+
+		Application app = new LwjglApplication(new BludBourneGame(), config);
+
+		Gdx.app = app;
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 	}
 }
